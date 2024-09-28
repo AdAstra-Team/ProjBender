e1c4cb6cc6cf43bf790491c0e292cf2dc699af31
Alexander Afonin <63101279+lueFlake@users.noreply.github.com>
2024-09-28 10:05:05 +0300

    user upd

--- server/CRM_hack/projects/src/main/java/org/example/models/entities/User.java
+++ server/CRM_hack/projects/src/main/java/org/example/models/entities/User.java
@@ -2,9 +2,14 @@ package org.example.models.entities;
 
 import jakarta.persistence.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @Entity
+@Table(name = "general_user", indexes = {
+        @Index(name = "idx_user_name", columnList = "name"),
+        @Index(name = "idx_user_authId", columnList = "authId")
+})
 public class User {
 
     @Id
@@ -12,6 +17,12 @@ public class User {
     private UUID id;
 
     private String name;
+
+    @OneToMany(mappedBy = "assignee")
+    private List<User> assignee;
+
+    @OneToMany(mappedBy = "author")
+    private List<Task> tasks;
 
     public UUID getId() {
         return id;
