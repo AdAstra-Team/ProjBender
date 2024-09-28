b8b2f85e2cfbd361c698f4a684a5f5a763353d6e
Alexander Afonin <63101279+lueFlake@users.noreply.github.com>
2024-09-28 10:08:58 +0300

    changed name of field

--- server/CRM_hack/projects/src/main/java/org/example/models/entities/User.java
+++ server/CRM_hack/projects/src/main/java/org/example/models/entities/User.java
@@ -19,7 +19,7 @@ public class User {
     private String name;
 
     @OneToMany(mappedBy = "assignee")
-    private List<User> assignee;
+    private List<User> assignedTasks;
 
     @OneToMany(mappedBy = "author")
     private List<Task> tasks;
