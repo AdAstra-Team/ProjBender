ad523f5bb58ffa60eb28df1b9f29305260476a1f
Savr123 <Sangievv@mail.ru>
2024-09-28 16:52:38 +0300

    Last hope

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navigation, Dashboard, DesksPage, TaskPage, TasksPage, TaskCreatePage, ProjectsPage, CalendarPage } from './Pages';
-import { TaskCard } from './Components';
+import { TaskCard, TaskCardList, TaskCardWithNodata } from './Components';
 import { Provider } from 'react-redux';
 import { store } from './app/Store';
 
@@ -19,10 +19,11 @@ function App() {
               <Routes>
                   <Route path="/" element={<Dashboard />} />
                   <Route path="/boards" element={<DesksPage />} />
-                  <Route path="/tasks" element={<TaskPage />} />
+                  <Route path="/tasks" element={<TaskCardList />} />
                   <Route path="/calendar" element={<CalendarPage />} />
                   <Route path="/projects" element={<ProjectsPage />} />
                   <Route path="/CreateTask" element={<TaskCreatePage />} />
+                  <Route path="/JustSimpleTaskDemo" element={<TaskCardWithNodata />} />
                   {/* <Route path="/Task{id}" element={<TaskCard />} /> */}
               </Routes>
           </Router>
--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -3,5 +3,6 @@ export {default as TaskList} from "./Task/TaskList"
 export {default as Project} from "./Project/Project"
 export {default as ProjectList} from "./Project/ProjectList"
 export {default as User} from "./User/User"
+export { TaskCardList, TaskCardWithNodata } from "./Task/TaskCard"
 export * from "./Task/TaskModel"
 
