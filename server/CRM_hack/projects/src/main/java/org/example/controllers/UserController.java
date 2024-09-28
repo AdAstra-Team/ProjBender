06ab1b0d32e8954c7fa189b21cd0f7fae7335744
Alexander Afonin <63101279+lueFlake@users.noreply.github.com>
2024-09-28 23:04:50 +0300

    user dto done

--- server/CRM_hack/projects/src/main/java/org/example/controllers/UserController.java
+++ server/CRM_hack/projects/src/main/java/org/example/controllers/UserController.java
@@ -1,9 +1,11 @@
 package org.example.controllers;
 
+import org.example.models.dao.UserValue;
 import org.example.models.entities.Task;
 import org.example.models.entities.User;
 import org.example.services.TaskService;
 import org.example.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,18 +21,20 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private ModelMapper mapper;
 
     @GetMapping
-    public User getMe() {
+    public UserValue getMe() {
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         var jwt = (Jwt)(authentication.getCredentials());
         var uuid = UUID.fromString(jwt.getClaim("sub"));
 
-        return userService.getUserByAuthId(uuid);
+        return mapper.map(userService.getUserByAuthId(uuid), UserValue.class);
     }
 
     @GetMapping("/{name}")
-    public User getUserByName(@PathVariable String name) {
-        return userService.getUserByName(name);
+    public UserValue getUserByName(@PathVariable String name) {
+        return mapper.map(userService.getUserByName(name), UserValue.class);
     }
 }
