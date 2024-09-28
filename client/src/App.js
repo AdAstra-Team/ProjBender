a31ff58c759585a3f99576f40d9944e85b56c4ce
Savr123 <Sangievv@mail.ru>
2024-09-28 18:51:50 +0300

    it's only keycloak integration, but Im not sure only

--- client/src/App.js
+++ client/src/App.js
@@ -3,31 +3,49 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navigation, Dashboard, DesksPage, TaskPage, TasksPage, TaskCreatePage, ProjectsPage, CalendarPage } from './Pages';
 import { TaskCard, TaskCardList, TaskCardWithNodata } from './Components';
-import { Provider } from 'react-redux';
-import { store } from './app/Store';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import keycloak from './Services/KeyCloak';
+import { setAuth, clearAuth } from './Features/Auth/authSlice';
 
 
 function App() {
+  const dispatch = useDispatch();
+  const [keycloakInitialized, setKeycloakInitialized] = useState(false);
+
+  useEffect(() => {
+    keycloak.init({ onLoad: 'login-required' }).then((authenticated) => {
+      if (authenticated) {
+        const token = keycloak.token;
+        const user = keycloak.tokenParsed;
+        
+        dispatch(setAuth({ token, user }));
+      } else {
+        dispatch(clearAuth());
+      }
+      setKeycloakInitialized(true);
+    });
+  }, [dispatch]);
+
+  if (!keycloakInitialized) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="App">
-      {/* <header className="App-header"> */}
-      {/* </header> */}
-
-      <Provider store={store}>
-        <Router>
-          <Navigation/>
-              <Routes>
-                  <Route path="/" element={<Dashboard />} />
-                  <Route path="/boards" element={<DesksPage />} />
-                  <Route path="/tasks" element={<TaskCardList />} />
-                  <Route path="/calendar" element={<CalendarPage />} />
-                  <Route path="/projects" element={<ProjectsPage />} />
-                  <Route path="/CreateTask" element={<TaskCreatePage />} />
-                  <Route path="/JustSimpleTaskDemo" element={<TaskCardWithNodata />} />
-                  {/* <Route path="/Task{id}" element={<TaskCard />} /> */}
-              </Routes>
-          </Router>
-        </Provider>
+      <Router>
+        <Navigation/>
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/boards" element={<DesksPage />} />
+                <Route path="/tasks" element={<TaskCardList />} />
+                <Route path="/calendar" element={<CalendarPage />} />
+                <Route path="/projects" element={<ProjectsPage />} />
+                <Route path="/CreateTask" element={<TaskCreatePage />} />
+                <Route path="/JustSimpleTaskDemo" element={<TaskCardWithNodata />} />
+                {/* <Route path="/Task{id}" element={<TaskCard />} /> */}
+            </Routes>
+        </Router>
     </div>
   );
 }
