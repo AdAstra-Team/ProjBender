b843cb337490b1fff0278817038f8f15cde966c3
zemlianin <edribnokhod@gmail.com>
2024-09-28 14:06:21 +0300

    Add auth to swagger

--- server/CRM_hack/projects/src/main/java/org/example/configurations/SwaggerConfiguration.java
+++ server/CRM_hack/projects/src/main/java/org/example/configurations/SwaggerConfiguration.java
@@ -1,11 +1,16 @@
 package org.example.configurations;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfiguration {
@@ -16,6 +21,18 @@ public class SwaggerConfiguration {
     @Bean
     public OpenAPI customOpenAPI() {
         OpenAPI openAPI = new OpenAPI()
+                .servers(List.of(
+                        new Server().url("https://ad-4stra.ru/api/").description("Production Server"),
+                        new Server().url("http://localhost:8082/api/").description("Local Development Server")
+                ))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization")))
                 .info(new Info()
                         .title("General API ProjBender ")
                         .version("1.0")
