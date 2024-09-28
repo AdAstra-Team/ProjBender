9ccc2d29247faa7138dbbe082339a71a62b94e4a
Savr123 <Sangievv@mail.ru>
2024-09-28 20:25:16 +0300

    fix: after login fix

--- client/src/app/Store.js
+++ client/src/app/Store.js
@@ -1,12 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import projectReducer from '../Features/Projects/ProjectSlice';
 import taskReducer from '../Features/Tasks/TaskSlice';
 import authReducer from '../Features/Auth/authSlice';
 
+// Combine all reducers
+const rootReducer = combineReducers({
+  auth: authReducer,
+  projects: projectReducer,
+  tasks: taskReducer,
+});
+
+// Create the Redux store
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    projects: projectReducer,
-    tasks: taskReducer,
-  },
+  reducer: rootReducer,
 });
